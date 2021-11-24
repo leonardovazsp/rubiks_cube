89184dda636016448333872ca6db5232d5aebee3
Leo Vaz <leonardovazsp@gmail.com>
2021-11-24 22:25:32 +1000

    changes to server.py

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 import socketserver
-import cube.py
+import cube
 
 cube = cube.Cube()
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        move = 'cube.'+ str(self.data, 'utf-8')
+        move = str(self.data, 'utf-8')
         print(move)
+        if move[-3:] == 'rev':
+            cube.orientation = 0
+            move = move[:-4]
+        else:
+            cube.orientation = 1
+        print(move)
+        move = getattr(cube, move)()
+        move
         self.request.sendall(self.data.upper())
 
 if __name__ == '__main__':
-    HOST, PORT = 'localhost', 9999
+    HOST, PORT = '192.168.1.107', 9999
 
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         server.serve_forever()
