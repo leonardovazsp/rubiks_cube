65a93ffd8c5e99b68fce4d00d5b44967972b6d4d
Leo Vaz <leonardovazsp@gmail.com>
2022-03-24 22:37:44 +1000

    added functions to training_pipeline.py

--- src/data/training_pipeline.py
+++ src/data/training_pipeline.py
@@ -7,6 +7,9 @@ import os
 import random
 from scipy import ndimage
 import copy
+import tensorflow as tf
+
+AUTOTUNE = tf.data.AUTOTUNE
 
 def augmentation(img,
                  brightness_range=(0.8, 1.2),
@@ -57,4 +60,51 @@ def create_dataset(directory_path, training_size=0.9, verbose=True):
         print(f'Training size: {tf.data.experimental.cardinality(train_ds).numpy()}')
         print(f'Validation size: {tf.data.experimental.cardinality(val_ds).numpy()}')
     
+    return train_ds, val_ds
+
+def get_label(file_path):
+    label = np.load(str(file_path.numpy(), 'utf-8')[:-6] + '.npy')
+    if str(file_path.numpy())[-6] == '2':
+        label = label[[2,3,5]]
+        label_ = copy.deepcopy(label)
+        label[0] = np.rot90(label_[1], 2)
+        label[1] = np.rot90(label_[0], 2)
+        label[2] = np.rot90(label_[2], 1)
+    else:
+        label = label[[0,1,4]]
+    label = label.reshape((27, 6))
+    label = tf.convert_to_tensor(label, dtype=tf.float32)
+    return label
+
+def decode_img(img):
+     
+    img = tf.io.decode_jpeg(img, channels=3)/255
+    img = tf.image.resize(img, [img_height, img_width])
+    img = tf.convert_to_tensor(img, dtype=tf.float32)
+    return img
+
+def process_path(file_path):
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    img = augmentation(img)
+    label = get_label(file_path)
+    return img, label
+
+def configure_for_performance(ds, batch_size):
+    ds = ds.cache()
+    ds = ds.shuffle(buffer_size=8)
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(buffer_size=AUTOTUNE)
+    return ds
+
+def wrapper_func(x):
+    x, y = tf.py_function(process_path, [x], [tf.float32, tf.float32])
+    return x, y
+
+def get_data_set():
+    train_ds, val_ds = create_dataset(path)
+    train_ds = train_ds.map(wrapper_func)
+    val_ds = val_ds.map(wrapper_func)
+    train_ds = configure_for_performance(train_ds)
+    val_ds = configure_for_performance(val_ds)
     return train_ds, val_ds
